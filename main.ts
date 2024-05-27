c9fc430cfe5713b4e3a7a357af9e521ab5053f5c
Anton Brall <info@antonbrall.de>
2024-05-27 16:36:25 +0200

    made the plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,98 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Plugin, FuzzySuggestModal, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AutoLinkRename extends Plugin {
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'add-link',
+			name: 'Add Link',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				const modal = new FilesModal(this.app, this, editor)
+                modal.open()
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
 	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	addlink = async (editor: Editor, files: TFile[]) => {
+		console.log(files[0])
+		const currentFile = files[0]
+		const data = this.app.metadataCache.getCache(currentFile["path"])
+		var linkName = undefined;
+		if (data && data["frontmatter"] && data["frontmatter"]["link-name"]) {
+			linkName = data["frontmatter"]["link-name"]
+		}
+		const currentFileLink = this.app.fileManager.generateMarkdownLink(currentFile, currentFile.path, undefined, linkName)
+		editor.replaceSelection(currentFileLink)
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class FilesModal extends FuzzySuggestModal<TFile> {
+    files: TFile[];
+    newNoteResult: HTMLDivElement;
+    suggestionEmpty: HTMLDivElement;
+    obsFile: any;
+    noSuggestion: boolean;
+    plugin: AutoLinkRename;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    EMPTY_TEXT = 'Files not found';
 
-	display(): void {
-		const {containerEl} = this;
+    constructor(app: App, plugin: AutoLinkRename, public editor: Editor) {
+        super(app);
+        this.plugin = plugin;
+		this.editor = editor;
+        this.init();
+    }
 
-		containerEl.empty();
+    init() {
+        this.files = this.app.vault.getMarkdownFiles();
+        this.emptyStateText = this.EMPTY_TEXT;
+        this.setInstructions(
+            [
+                {command: '↑↓', purpose: 'to navigate'},
+                {command: '↵', purpose: 'to append link to the file'},
+                {command: 'esc', purpose: 'to dismiss'}
+            ]
+        );
+        this.initNewNoteItem();
+    }
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    getItems(): TFile[] {
+        return this.files;
+    }
+
+    getItemText(item: TFile): string {
+        this.noSuggestion = false;
+        return item.basename;
+    }
+
+    onNoSuggestion() {
+        this.noSuggestion = true;
+    }
+
+    onChooseItem(item: TFile, evt: MouseEvent | KeyboardEvent): void {
+        if (this.noSuggestion) {
+            // this.modalNoteCreation.create(this.inputEl.value);
+        } else {
+            this.plugin.addlink(this.editor, [item]);
+        }
+    }
+
+    initNewNoteItem() {
+        this.newNoteResult = document.createElement('div');
+        this.newNoteResult.addClasses(['suggestion-item', 'is-selected']);
+        this.suggestionEmpty = document.createElement('div');
+        this.suggestionEmpty.addClass('suggestion-empty');
+        this.suggestionEmpty.innerText = this.EMPTY_TEXT;
+    }
+
+    itemInstructionMessage(resultEl: HTMLElement, message: string) {
+        const el = document.createElement('kbd');
+        el.addClass('suggestion-hotkey');
+        el.innerText = message;
+        resultEl.appendChild(el);
+    }
+
 }
