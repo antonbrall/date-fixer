adc4dc320aad4c6c4af57dedf27996ec4a8ef1d7
Anton Brall <info@antonbrall.de>
2024-07-17 12:05:19 +0200

    did the whole thingy

--- main.ts
+++ main.ts
@@ -1,14 +1,26 @@
 import { App, Editor, MarkdownView, Plugin, FuzzySuggestModal, TFile } from 'obsidian';
 
-export default class AutoLinkRename extends Plugin {
+
+export default class dateFixer extends Plugin {
 
 	async onload() {
 		this.addCommand({
-			id: 'add-link',
-			name: 'Add Link',
+			id: 'add-dates',
+			name: 'Add Dates',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				const modal = new FilesModal(this.app, this, editor)
-                modal.open()
+                console.log("editor title?")
+                console.log(view.file?.basename)
+                if (view.file) {
+                    const filename = view.file.basename
+                    const date = new Date(filename)
+                    const yesterday = this.addDays(date, -1)
+                    const tomorrow = this.addDays(date, 1)
+                    const yesterdayString = yesterday.toISOString().split("T")[0]
+                    const tomorrowString = tomorrow.toISOString().split("T")[0]
+                    editor.replaceSelection(`[[${yesterdayString}|Gestern]] [[${tomorrowString}|Morgen]]`)
+                }
+                
+                
 			}
 		});
 	}
@@ -16,83 +28,10 @@ export default class AutoLinkRename extends Plugin {
 	onunload() {
 
 	}
-	addlink = async (editor: Editor, files: TFile[]) => {
-		console.log(files[0])
-		const currentFile = files[0]
-		const data = this.app.metadataCache.getCache(currentFile["path"])
-		var linkName = undefined;
-		if (data && data["frontmatter"] && data["frontmatter"]["link-name"]) {
-			linkName = data["frontmatter"]["link-name"]
-		}
-		const currentFileLink = this.app.fileManager.generateMarkdownLink(currentFile, currentFile.path, undefined, linkName)
-		editor.replaceSelection(currentFileLink)
-	}
+    addDays(date: Date, days: number): Date {
+        let result = new Date(date);
+        result.setDate(date.getDate() + days);
+        return result;
+    }
 }
 
-class FilesModal extends FuzzySuggestModal<TFile> {
-    files: TFile[];
-    newNoteResult: HTMLDivElement;
-    suggestionEmpty: HTMLDivElement;
-    obsFile: any;
-    noSuggestion: boolean;
-    plugin: AutoLinkRename;
-
-    EMPTY_TEXT = 'Files not found';
-
-    constructor(app: App, plugin: AutoLinkRename, public editor: Editor) {
-        super(app);
-        this.plugin = plugin;
-		this.editor = editor;
-        this.init();
-    }
-
-    init() {
-        this.files = this.app.vault.getMarkdownFiles();
-        this.emptyStateText = this.EMPTY_TEXT;
-        this.setInstructions(
-            [
-                {command: '↑↓', purpose: 'to navigate'},
-                {command: '↵', purpose: 'to append link to the file'},
-                {command: 'esc', purpose: 'to dismiss'}
-            ]
-        );
-        this.initNewNoteItem();
-    }
-
-    getItems(): TFile[] {
-        return this.files;
-    }
-
-    getItemText(item: TFile): string {
-        this.noSuggestion = false;
-        return item.basename;
-    }
-
-    onNoSuggestion() {
-        this.noSuggestion = true;
-    }
-
-    onChooseItem(item: TFile, evt: MouseEvent | KeyboardEvent): void {
-        if (this.noSuggestion) {
-            // this.modalNoteCreation.create(this.inputEl.value);
-        } else {
-            this.plugin.addlink(this.editor, [item]);
-        }
-    }
-
-    initNewNoteItem() {
-        this.newNoteResult = document.createElement('div');
-        this.newNoteResult.addClasses(['suggestion-item', 'is-selected']);
-        this.suggestionEmpty = document.createElement('div');
-        this.suggestionEmpty.addClass('suggestion-empty');
-        this.suggestionEmpty.innerText = this.EMPTY_TEXT;
-    }
-
-    itemInstructionMessage(resultEl: HTMLElement, message: string) {
-        const el = document.createElement('kbd');
-        el.addClass('suggestion-hotkey');
-        el.innerText = message;
-        resultEl.appendChild(el);
-    }
-
-}
